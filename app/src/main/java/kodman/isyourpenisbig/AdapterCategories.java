373c2521434792ae40fb371f336551e43f3cfe18
CodmanD <korovin.dmitry.1977@gmail.com>
2018-07-06 19:13:08 +0300

    06.07/3

---
+++ app/src/main/java/kodman/isyourpenisbig/AdapterCategories.java
@@ -0,0 +1,121 @@
+package kodman.isyourpenisbig;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+
+
+/**
+ * Created by DI1 on 19.03.2018.
+ */
+
+public class AdapterCategories extends RecyclerView.Adapter<AdapterCategories.ViewHolder> {
+
+    private List<String> items;
+    private Context context;
+    LayoutInflater inflater;
+
+    public AdapterCategories(Context context, LayoutInflater inflater, final List<String> items) {
+        this.context=context;
+        this.items = items;
+    }
+
+
+    @Override
+    public AdapterCategories.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+
+        inflater= LayoutInflater.from(viewGroup.getContext());
+        View v = inflater.inflate(R.layout.item_country, viewGroup, false);
+
+        return new AdapterCategories.ViewHolder(v);
+    }
+
+
+    @Override
+    public void onBindViewHolder(AdapterCategories.ViewHolder viewHolder, int i) {
+
+        String category = items.get(i);
+        viewHolder.tv.setText(category);
+        viewHolder.position=i;
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView tv;
+        private int position;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            this.tv = (TextView) itemView.findViewById(R.id.tvCategory);
+            tv.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+//                    AlertDialog.Builder builder=new AlertDialog.Builder(context);
+//                    final View view=inflater.inflate(R.layout.dialog_category,null);
+//                    final EditText et=view.findViewById(R.id.etCategory);
+//                    et.setText(tv.getText());
+//                    builder
+//                            .setCancelable(true)
+//                            .setView(view)
+//                            .setTitle(R.string.newCategory)
+//                            .setPositiveButton(R.string.changeCategory, new DialogInterface.OnClickListener() {
+//                        @Override
+//                        public void onClick(DialogInterface dialog, int which) {
+//
+//                            long res= DatabaseHelper.getInstance(context).updateCategory(et.getText().toString(),tv.getText().toString());
+//                            tv.setText(et.getText());
+//                            Toast.makeText(context,context.getResources().getString(R.string.deleted), Toast.LENGTH_SHORT).show();
+//
+//                            dialog.cancel();
+//                            categories.add(position,tv.getText().toString());
+//                            categories.remove(position+1);
+//                            AdapterCategories.this.notifyDataSetChanged();
+//                        }
+//                    })
+//                            .setNeutralButton(R.string.buttonCancel,new DialogInterface.OnClickListener()
+//                    { @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                       // Toast.makeText(context,"Cancel",Toast.LENGTH_SHORT).show();
+//                        dialog.cancel();
+//                    }
+//                    })
+//                            .setNegativeButton(R.string.buttonDeleteCategory,new DialogInterface.OnClickListener()
+//                            { @Override
+//                            public void onClick(DialogInterface dialog, int which) {
+//                                long res= DatabaseHelper.getInstance(context).removeCategory(et.getText().toString());
+//                                Toast.makeText(context,context.getResources().getString(R.string.deleted), Toast.LENGTH_SHORT).show();
+//                                dialog.cancel();
+//                                categories.remove(position);
+//                                AdapterCategories.this.notifyDataSetChanged();
+//                            }
+//                            })
+//
+//
+//                    .create().show();
+                }
+            });
+        }
+    }
+}
--- app/src/main/java/kodman/isyourpenisbig/MainActivity.java
+++ app/src/main/java/kodman/isyourpenisbig/MainActivity.java
@@ -1,19 +1,25 @@
 package kodman.isyourpenisbig;
 
 import android.content.Context;
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.MobileAds;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -63,8 +69,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
        // setContentView(R.layout.activity_main);
         setContentView(R.layout.screen_conditions);
-        GridView gridview = (GridView) findViewById(R.id.gridview);
-        gridview.setAdapter(new ImageAdapter(this));
+Spinner spinner=this.findViewById(R.id.spinner);
+        String[] countries= getResources().getStringArray(R.array.countries);
+         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.spin, R.id.tvCategory,countries);
+        adapter.setDropDownViewResource(R.layout.spin_dropdown);
+
+
+      //  if (themeNumber == 1 && Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+        //    spinner.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.spinner_blue));
+
+        spinner.setAdapter(adapter);
 //        ImageView iv=this.findViewById(R.id.imageView);
 //        TextView tv=this.findViewById(R.id.tvTitle2);
 //        View vLine=this.findViewById(R.id.vLine);
